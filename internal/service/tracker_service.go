a6eb3d27696eac6f5e893d9986972de51d8f8fe7
TomRomeo <tom.romeo.doil@gmail.com>
2025-05-02 10:05:43 +0200

    fix: add helper functions to create new snapshots and patches

--- internal/service/tracker_service.go
+++ internal/service/tracker_service.go
@@ -95,7 +95,7 @@ func (t *TrackerService) Commit(
 				return 0, err
 			}
 
-			snapshot := store.Snapshot{Object: newObject.Object, Time: time.Now()}
+			snapshot := newSnapshot(newObject, 0)
 			if err := t.rps.SetSnapshot(ctx, objID, &snapshot); err != nil {
 				return 0, err
 			}
@@ -121,11 +121,7 @@ func (t *TrackerService) Commit(
 	patchesSince := uint64(ts.rev) % t.snapshotEvery
 	if patchesSince == t.snapshotEvery-1 {
 		// we are at the end of a snapshot period, so we should create a new snapshot
-		snapshot := store.Snapshot{
-			PreviousID: ts.rev,
-			Object:     newObject.Object,
-			Time:       time.Now(),
-		}
+		snapshot := newSnapshot(newObject, ts.rev)
 		err := t.rps.SetSnapshot(ctx, objID, &snapshot)
 		if err != nil {
 			return 0, err
@@ -139,12 +135,8 @@ func (t *TrackerService) Commit(
 	}
 
 	diff := diffmap.Diff(ts.obj, newObject.Object)
-	p := &store.Patch{
-		PreviousID: ts.rev,
-		Patch:      diff,
-		Time:       time.Now(),
-	}
-	err := t.rps.SetPatch(ctx, objID, p)
+	p := newPatch(ts.rev, diff)
+	err := t.rps.SetPatch(ctx, objID, &p)
 	if err != nil {
 		return 0, err
 	}
@@ -155,6 +147,19 @@ func (t *TrackerService) Commit(
 	ts.rev = p.ID
 
 	return p.ID, nil
+}
+
+func newPatch(previousID store.RevisionID, diff diffmap.DiffMap) store.Patch {
+	return store.Patch{
+		PreviousID: previousID,
+		Patch:      diff,
+		Time:       time.Now(),
+	}
+}
+
+func newSnapshot(newObject *unstructured.Unstructured, previousID store.RevisionID) store.Snapshot {
+	snapshot := store.Snapshot{PreviousID: previousID, Object: newObject.Object, Time: time.Now()}
+	return snapshot
 }
 
 // Restore brings back the object state at *rev*.
